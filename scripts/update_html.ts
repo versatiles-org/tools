6d10b8a4ba10c29b4d09063ff1d1f35954f3c677
Michael Kreil <github@michael-kreil.de>
2025-06-18 10:38:26 +0200

    build: use cheerio_cms

--- scripts/update_html.ts
+++ scripts/update_html.ts
@@ -1,24 +1,16 @@
-import { mkdirSync, rmSync, writeFileSync } from 'node:fs';
+import { writeFileSync } from 'node:fs';
+import { Page } from 'cheerio_cms';
 
 process.chdir(new URL('../', import.meta.url).pathname);
 
-mkdirSync('scripts/lib', { recursive: true });
-const lib = await (
-	await fetch(
-		'https://raw.githubusercontent.com/versatiles-org/versatiles-org.github.io/refs/heads/main/src/cms/page.ts'
-	)
-).text();
-writeFileSync('scripts/lib/page.ts', lib);
-
-const { Page } = await import('./lib/page.js');
-
 let html = await (await fetch('https://versatiles.org/tools.html')).text();
-html = new Page(html)
+
+const page = new Page(html)
 	.setBaseUrl('https://versatiles.org/tools/')
 	.addHead('%sveltekit.head%')
-	.setContent('%sveltekit.body%')
-	.render();
+	.setContent('%sveltekit.body%');
+page.$('main').removeClass('markdown-body');
+
+html = page.render();
 
 writeFileSync('src/app.html', html);
-
-rmSync('scripts/lib', { recursive: true });
