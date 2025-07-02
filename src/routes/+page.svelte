d8a3e2efacf3c14b460ebfe537f4ce9da9c28bb2
Michael Kreil <github@michael-kreil.de>
2025-07-02 19:00:44 +0200

    fix: layout and styling on the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,34 @@
 <h1>VersaTiles Tools</h1>
 <p>We've prepared some tools to help you get started with VersaTiles:</p>
-<ul>
-	<li><a href="setup_server">Setup a new server</a></li>
-	<li><a href="bounding_box">Find a bounding box</a></li>
-</ul>
+<a class="tool" href="setup_server">
+	<span class="title">Setup a new VersaTiles server</span><br />
+	A quick, interactive guide to installing a VersaTiles map server in seconds, complete with map data
+	and front end.
+</a>
+
+<a class="tool" href="bounding_box">
+	<span class="title">Find a bounding box</span>
+</a>
+
+<style>
+	a.tool {
+		display: block;
+		margin: 2rem 0;
+		padding: 2rem;
+		border-radius: 0.5rem;
+		border: 1px solid rgba(255, 255, 255, 0.3);
+		text-decoration: none;
+	}
+
+	a.tool:hover {
+		border-color: rgba(255, 255, 255, 0.6);
+	}
+
+	.title {
+		font-size: 1.2rem;
+		display: block;
+		text-align: center;
+		font-weight: bold;
+		margin-bottom: 0.5rem;
+	}
+</style>
